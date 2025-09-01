62cdc3883bcc80374e3657f162f365e5fa5dfb59
juancarlos2v <juanc.vilcherrez@gmail.com>
2025-09-01 01:47:39 -0300

    ✨  funciones: ejercicio 2

--- funciones.py
+++ funciones.py
@@ -4,4 +4,20 @@ def area_rectangulo (base, altura):
     return base*altura
 
 area= area_rectangulo(2,3)
+print(f"El area del rectangulo con base 3 y altura 2 es {area}")
 
+# Realizar una función que se llame relacion() que a partir de dos nros realice lo siguiente:
+# a. Si el primer nro es mayor que el segundo, devuelve 1
+# b. Si el primer nro es menor que el segundo, devuelve -1
+# c. Si ambos nros son iguales, devuelve 0
+
+def relacion(a,b):
+    if a>b:
+        return 1
+    if a<b:
+        return -1
+    if a==b:
+        return 0
+
+rel=relacion(2,2)
+print(rel)
