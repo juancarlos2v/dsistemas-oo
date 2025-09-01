31472b504d8efb76a424b664ee3001b0d8b134b7
juancarlos2v <juanc.vilcherrez@gmail.com>
2025-09-01 01:44:53 -0300

    ✨  funciones: ejercicio 1

---
+++ funciones.py
@@ -0,0 +1,7 @@
+# Realizar una función que se llame area_rectangulo() que devuelva el área del rectángulo a partir de su base y altura.
+
+def area_rectangulo (base, altura):
+    return base*altura
+
+area= area_rectangulo(2,3)
+
