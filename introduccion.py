1cc8cb11b534fc8dcd4a1f85e4689c4a9da5c646
juancarlos2v <juanc.vilcherrez@gmail.com>
2025-09-01 00:58:42 -0300

    ✨  introduccion: ejercicio 2

--- introduccion.py
+++ introduccion.py
@@ -1,7 +1,15 @@
 # Definir dos variables de distinto valor numérico que ejecuten una operación  
 # finalmente se muestren por pantalla.
-
 a=13
 b=6
 
 print(a+b)
+
+# Definir dos variables con diferentes cadenas de texto que se concatenen
+# finalmente se muestren por pantalla.
+
+c="Hola"
+d="Mundo"
+
+print(f"{c} {d}")
+ 
