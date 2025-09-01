855623c926454d645aa801297682a76b9b6cba34
juancarlos2v <juanc.vilcherrez@gmail.com>
2025-09-01 01:03:04 -0300

    ✨  introduccion: ejercicio 3

--- introduccion.py
+++ introduccion.py
@@ -13,3 +13,11 @@ d="Mundo"
 
 print(f"{c} {d}")
  
+
+# Solicitar que el usuario ingrese Nombre y DNI, guardarlo en distintas variables e imprimir por
+# pantalla “El nombre del usuario es XXXXXX y su DNI es YYYYYY”.
+
+nombre= input("Ingresar nombre : ")
+dni=input("Ingresar DNI: ")
+
+print(f"El nombre del usuario es {nombre} y su DNI es {dni}")
