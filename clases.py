2699190dcb7332067a145bd94cdad7fd58c52dcc
Juan Carlos Vilcherrez <juanc.vilcherrez@gmail.com>
2025-09-15 21:11:29 -0300

    🚧  clases: ejercicio 4

--- clases.py
+++ clases.py
@@ -80,4 +80,37 @@ alumno.presentarse()
 alumno.estudiar()
 alumno.cambiar_carrera("Analisis de Sistemas")
 alumno.estudiar()
+
+print("============================================================")
+
+#Diseña una clase Libro con atributos titulo y autor. Incluye un método leer (debe imprimir que el libro está siendo leído). 
+#Crea una subclase Ebook que herede de Libro y añada un atributo formato y un método descargar (debe imprimir que el ebook está siendo descargado). 
+#Implementa un método adicional cambiar_formato que permita cambiar el formato del ebook, verificando que el nuevo formato sea válido (por ejemplo, "PDF", "EPUB", "MOBI").
+
+class Libro:
+    def __init__(self,titulo, autor):
+        self.titulo=titulo
+        self.autor=autor
     
+    def leer(self):
+        print(f"Estas leyendo {self.titulo} de {self.autor}")
+
+class Ebook(Libro):
+    def __init__(self, titulo, autor,formato):
+        super().__init__(titulo, autor)
+        self.formato=formato
+
+    def descargar(self):
+        print(f"Se esta descargando el libro {self.titulo} de {self.autor}")
+
+    def cambiar_formato(self,formato):
+        if(formato!="PDF" and formato != "EPUB" and formato!="MOBI"):
+            print("Formato no permitido")
+        else:
+            self.formato=formato
+            print(f"¡Formato cambiado a {self.formato} con exito!")
+
+libro=Ebook("Pandenomics","Javier Milei","PDF")
+libro.leer()
+libro.descargar()
+libro.cambiar_formato("AAA")
